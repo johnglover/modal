8e178688bda4b9432375b2260d7c2a81193d7116
John Glover <john.c.glover@nuim.ie>
2011-12-23 16:00:35 +0000

    Remove analysis and results from package list. Add scripts.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ The database is a hierarchical database, stored in the HDF5 format.
 """
 from setuptools import setup, Extension
 
-# get numpy include directory
 try:
     import numpy
     try:
@@ -27,25 +26,31 @@ except ImportError:
     exit(1)
 
 doc_lines = __doc__.split("\n")
-detectionfunctions = Extension("modal/detectionfunctions/_pydetectionfunctions", 
-                               sources=["modal/detectionfunctions/detectionfunctions.cpp", 
-                                        "modal/detectionfunctions/mq.cpp", 
-                                        "modal/detectionfunctions/detectionfunctions.i"],
-                               include_dirs=[numpy_include, '/usr/local/include'],
-                               libraries=['m', 'fftw3'],
-                               swig_opts=['-c++']) 
 
-setup(name='modal',
-      description=doc_lines[0],
-      long_description="\n".join(doc_lines[2:]),
-      url='http://github.com/johnglover/modal',
-      download_url='http://github.com/johnglover/modal',
-      license='GPL',
-      author='John Glover',
-      author_email='john.c.glover@nuim.ie',
-      platforms=["Linux", "Mac OS-X", "Unix", "Windows"],
-      version='1.11',
-      # requires=['numpy (>=1.0.1)'],
-      ext_modules=[detectionfunctions],
-      packages=['modal', 'modal.analysis', 'modal.data', 'modal.detectionfunctions',
-                'modal.results', 'modal.ui', 'modal.utils'])
+detectionfunctions = Extension(
+    "modal/detectionfunctions/_pydetectionfunctions", 
+    sources=[
+        "modal/detectionfunctions/detectionfunctions.cpp", 
+        "modal/detectionfunctions/mq.cpp", 
+        "modal/detectionfunctions/detectionfunctions.i"
+    ],
+    include_dirs=[numpy_include, '/usr/local/include'],
+    libraries=['m', 'fftw3'],
+    swig_opts=['-c++']
+) 
+
+setup(
+    name='modal',
+    description=doc_lines[0],
+    long_description="\n".join(doc_lines[2:]),
+    url='http://github.com/johnglover/modal',
+    download_url='http://github.com/johnglover/modal',
+    license='GPL',
+    author='John Glover',
+    author_email='john.c.glover@nuim.ie',
+    platforms=["Linux", "Mac OS-X", "Unix", "Windows"],
+    version='1.11',
+    ext_modules=[detectionfunctions],
+    packages=['modal', 'modal.data', 'modal.detectionfunctions', 'modal.ui', 'modal.utils'],
+    scripts = ['bin/editonsets', 'bin/modaldb2audio']
+)
