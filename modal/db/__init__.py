31fd2fc0bed10d0491c1ed21225e768d532648c3
John Glover <j@johnglover.net>
2012-01-27 15:16:03 +0000

    [db] add samples function for returning sample and metadata info.
    Deprecate functions for listing db file names.

--- modal/db/__init__.py
+++ modal/db/__init__.py
@@ -7,56 +7,74 @@ onsets_path = os.environ.get(
     'MODAL_ONSETS_PATH', os.path.join(data_path, "onsets.hdf5")
 )
 
-# list all files in the onsets database
+def _sample_and_metadata(db, file_name):
+    """
+    Return a dict containing a copy of a sample and all of its metadata.
+    """
+    data = {'name': file_name}
+    data['samples'] = np.array(db[file_name], dtype=np.double)
+    for attribute, value in db[file_name].attrs.iteritems():
+        data[attribute] = value
+    return data
+
+def samples(file_name=None, attribute_name=None, attribute_value=None):
+    """
+    Return samples from Modal database.
+
+    If a file name is given, returns a dict containing the given sample and its metadata.
+    If not, returns a dict containing dicts of samples and the corresponding metadata.
+    Samples can optionally be filtered by attribute name and value.
+    """
+    samples = {}
+    db = None
+    try:
+        db = h5py.File(onsets_path, 'r')
+        if file_name:
+            samples = _sample_and_metadata(db, file_name)
+        else:
+            for f in db:
+                if attribute_name and attribute_value:
+                    if db[f].attrs[attribute_name] != attribute_value:
+                        continue
+                samples[f] = _sample_and_metadata(db, f)
+    finally:
+        if db: db.close()
+
+    return samples
+
 def list_onset_files():
-    try:
-        onsets_db = h5py.File(onsets_path, 'r')
-        return list(onsets_db)
-    except:
-        return []
-    finally:
-        onsets_db.close()
+    """
+    List names of all files in the onsets database.
+    Note: This is now deprecated in favour of modal.db.samples()
+    """
+    print "Deprecation Warning: list_onset_files() is now deprecated in favour of modal.db.samples()"
+    return samples().keys()
 
-# list polyphonic files
 def list_onset_files_poly():
-    try:
-        files = []
-        onsets_db = h5py.File(onsets_path, 'r')
-        for file in onsets_db:
-            if onsets_db[file].attrs['texture'] == "Polyphonic":
-                files.append(file)
-        return files
-    except:
-        return []
-    finally:
-        onsets_db.close()
+    """
+    List names of all polyphonic files in the database.
+    Note: list_onset_files_poly() is now deprecated in favour of modal.db.samples()
+    """
+    print "Deprecation Warning: This function is now deprecated in favour of modal.db.samples()"
+    return samples(None, 'texture', 'Polyphonic').keys()
 
-# get the current number of onsets in the onsets database
 def num_onsets():
+    """
+    Get the current number of onsets in the database.
+    """
+    num_onsets = 0
     try:
-        num_onsets = 0
         onsets_db = h5py.File(onsets_path, 'r')
         for file in onsets_db:
             num_onsets += len(onsets_db[file].attrs['onsets'])
-        return num_onsets
-    except:
-        return 0
     finally:
         onsets_db.close()
+    return num_onsets
 
-# get a given audio file from the onset database
 def get_audio_file(file_name):
-    audio = np.array([])
-    sampling_rate = 0.0
-    onsets = np.array([])
-    try:
-        onsets_db = h5py.File(onsets_path, 'r')
-        if file_name in onsets_db:
-            # create new arrays so we can close the database connection
-            audio = np.array(onsets_db[file_name], dtype=np.double)
-            sampling_rate = int(onsets_db[file_name].attrs['sampling_rate'])
-            onsets = np.array(onsets_db[file_name].attrs['onsets'])
-    finally:
-        onsets_db.close()
-        return (audio, sampling_rate, onsets)
+    """
+    Get a given audio file from the onset database.
+    """
+    sample = samples(file_name)
+    return (sample['samples'], sample['sampling_rate'], sample['onsets'])
 
