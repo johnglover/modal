ef70bf901e5e6fa558f3c5822c6231470acf4285
John Glover <j@johnglover.net>
2012-04-19 16:30:29 +0100

    [setup, ui] whitespace clean up

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 """Modal: Musical Onset Database And Library.
 
-Modal is a cross-platform library for musical onset detection, written in C++ and Python.
-It is provided here under the terms of the GNU General Public License.
-It consists of code for several different types of Onset Detection Function (ODF) and code for
-real-time and non-real-time onset detection.
+Modal is a cross-platform library for musical onset detection, written in C++
+and Python. It is provided here under the terms of the GNU General Public
+License. It consists of code for several different types of Onset Detection
+Function (ODF) and code for real-time and non-real-time onset detection.
 
-All ODFs are implemented in both C++ and Python. The code for Onset detection and ODF comparison
-is currently only available in Python.
+All ODFs are implemented in both C++ and Python. The code for Onset detection
+and ODF comparison is currently only available in Python.
 
-Modal also includes a database of musical samples with hand-annotated onset locations for ODF 
-evaluation purposes. The database can be found in the downloads section, it is not included 
-in the repository. 
+Modal also includes a database of musical samples with hand-annotated onset
+locations for ODF evaluation purposes. The database can be found in the
+downloads section, it is not included in the repository.
 The database is a hierarchical database, stored in the HDF5 format.
 """
 from setuptools import setup, Extension
@@ -28,16 +28,16 @@ except ImportError:
 doc_lines = __doc__.split("\n")
 
 detectionfunctions = Extension(
-    "modal/detectionfunctions/_pydetectionfunctions", 
+    "modal/detectionfunctions/_pydetectionfunctions",
     sources=[
-        "modal/detectionfunctions/detectionfunctions.cpp", 
-        "modal/detectionfunctions/mq.cpp", 
+        "modal/detectionfunctions/detectionfunctions.cpp",
+        "modal/detectionfunctions/mq.cpp",
         "modal/detectionfunctions/detectionfunctions.i"
     ],
     include_dirs=[numpy_include, '/usr/local/include'],
     libraries=['m', 'fftw3'],
     swig_opts=['-c++']
-) 
+)
 
 setup(
     name='modal',
@@ -51,6 +51,7 @@ setup(
     platforms=["Linux", "Mac OS-X", "Unix", "Windows"],
     version='1.11',
     ext_modules=[detectionfunctions],
-    packages=['modal', 'modal.db', 'modal.detectionfunctions', 'modal.ui', 'modal.utils'],
-    scripts = ['bin/editonsets', 'bin/modalimport', 'bin/modalexport']
+    packages=['modal', 'modal.db', 'modal.detectionfunctions',
+              'modal.ui', 'modal.utils'],
+    scripts=['bin/editonsets', 'bin/modalimport', 'bin/modalexport']
 )
