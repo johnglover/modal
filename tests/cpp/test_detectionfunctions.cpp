a4215b3680de395cc405d85650e0abd2615bcfe0
John Glover <j@johnglover.net>
2013-02-15 09:56:56 +0100

    [tests] Add test for hop size = frame size

--- tests/cpp/test_detectionfunctions.cpp
+++ tests/cpp/test_detectionfunctions.cpp
@@ -14,17 +14,17 @@ void TestPeakODF::setUp() {
 void TestPeakODF::test_basic() {
     std::vector<sample> audio(_sf.frames(), 0.0);
     _sf.read(&audio[0], (int)_sf.frames());
-    
+
     int hop_size = _odf.get_hop_size();
     int frame_size = _odf.get_frame_size();
     std::vector<sample> odf_values;
 
     for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
-        odf_values.push_back(_odf.process_frame(frame_size, &(audio[i]))); 
+        odf_values.push_back(_odf.process_frame(frame_size, &(audio[i])));
     }
 
     CPPUNIT_ASSERT(odf_values.size() > 0);
-    
+
     for(int i = 0; i < odf_values.size(); i++) {
         CPPUNIT_ASSERT(odf_values[i] >= 0.0);
     }
@@ -42,11 +42,29 @@ void TestPeakODF::test_changing_hop_frame_sizes() {
     _odf.set_frame_size(frame_size);
 
     for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
-        odf_values.push_back(_odf.process_frame(frame_size, &(audio[i]))); 
+        odf_values.push_back(_odf.process_frame(frame_size, &(audio[i])));
     }
 
     CPPUNIT_ASSERT(odf_values.size() > 0);
-    
+
+    for(int i = 0; i < odf_values.size(); i++) {
+        CPPUNIT_ASSERT(odf_values[i] >= 0.0);
+    }
+
+    hop_size = 256;
+    frame_size = 256;
+    odf_values.clear();
+
+    _odf.reset();
+    _odf.set_hop_size(hop_size);
+    _odf.set_frame_size(frame_size);
+
+    for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
+        odf_values.push_back(_odf.process_frame(frame_size, &(audio[i])));
+    }
+
+    CPPUNIT_ASSERT(odf_values.size() > 0);
+
     for(int i = 0; i < odf_values.size(); i++) {
         CPPUNIT_ASSERT(odf_values[i] >= 0.0);
     }
@@ -60,11 +78,11 @@ void TestPeakODF::test_changing_hop_frame_sizes() {
     _odf.set_frame_size(frame_size);
 
     for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
-        odf_values.push_back(_odf.process_frame(frame_size, &(audio[i]))); 
+        odf_values.push_back(_odf.process_frame(frame_size, &(audio[i])));
     }
 
     CPPUNIT_ASSERT(odf_values.size() > 0);
-    
+
     for(int i = 0; i < odf_values.size(); i++) {
         CPPUNIT_ASSERT(odf_values[i] >= 0.0);
     }
